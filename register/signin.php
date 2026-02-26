7a9192fbd7a7fdc3e94d681849c421069814047d
KARTIKEYSHARMA18 <152916037+KARTIKEYSHARMA18@users.noreply.github.com>
2026-02-26 21:26:27 +0530

    user can register now

--- register/signin.php
+++ register/signin.php
@@ -1,9 +1,11 @@
 <?php
 $errors = [];
-$name = $email="";
+$name=$email=$user_password=$confirm_password="";
 if($_SERVER["REQUEST_METHOD"]=="POST"){
     $name = trim($_POST['name']);
     $email = trim($_POST['email']);
+    $user_password = $_POST['password'] ?? '';
+    $confirm_password=$_POST['confirm_password'] ?? '';
     if($name===''){
         $errors['name'] = 'name is required.' ;
     }
@@ -14,11 +16,25 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
     else if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
        $errors['email'] = "invalid email format";
     }
+    if($user_password===''){
+        $errors['password'] = "password is req.";
+    }
+    else if(strlen($user_password)<6){
+        $errors['password'] = "password must be at least 6 characters";
+    }
+    if($confirm_password === ""){
+        $errors['confirm_password'] = "confirm your password";
+
+    }
+    else if($user_password!== $confirm_password){
+        $errors['confirm_password'] = "password do not match";
+    }
+
     if(empty($errors)){
         require_once __DIR__ . '/../db.php';
-        
-        $stmt = mysqli_prepare($conn, "INSERT INTO users (name, email) VALUES (?, ?)");
-        mysqli_stmt_bind_param($stmt, "ss", $name, $email);
+        $hashedpassword = password_hash($user_password, PASSWORD_DEFAULT);
+        $stmt = mysqli_prepare($conn, "INSERT INTO users (name, email, password) VALUES (?, ?, ?)");
+        mysqli_stmt_bind_param($stmt, "sss", $name, $email, $hashedpassword);
         
         if(mysqli_stmt_execute($stmt)){
             header("Location: signin.php?success=1");
@@ -60,9 +76,14 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
 <?php endif; ?>
 <form action = "<?=  htmlspecialchars($_SERVER['PHP_SELF']) ?>" method = "post">
     <input type = "text" name = "name" value = "<?= htmlspecialchars($name) ?>"placeholder = "enter your name " required>
+    <span style="color:red"><?= $errors['name'] ?? '' ?></span>
    
     <input type="email" name="email" value="<?= htmlspecialchars($email) ?>" placeholder="Enter your email" required>
-   
+    <span style="color:red"><?= $errors['email'] ?? '' ?></span>
+    <input type = "password" name = "password" placeholder="enter password">
+    <span style="color:red"><?= $errors['password'] ?? '' ?></span>
+    <input type = "password" name = "confirm_password" placeholder="confirm password">
+    <span style="color:red"><?= $errors['confirm_password'] ?? '' ?></span>
       <button class="btn" >Submit</button>
 
 </form>
